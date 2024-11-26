185ae76fa70d15d6952181a0aa28811860a1296f
Danny Fu <43212622+DannyFu9993@users.noreply.github.com>
2024-11-25 23:47:33 -0500

    Added login HTML and CSS
    No working code yet, just HTML and CSS.

--- src/Pages/LoginSingnUp.jsx
+++ src/Pages/LoginSingnUp.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-const LoginSingnUp = () => {
+const LoginSignUp = () => {
   return (
-    <div>LoginSingnUp</div>
-  )
-}
+    <div class="LoginSignUp">
+      <h1>Login/Sign Up</h1><br></br><br></br><br></br>
+      
+      <label htmlFor="fname">Name: </label>
+      <input type="text" id="fname" name="fname" placeholder="John" /><br/>
+    
+      <label htmlFor="fname">Email address: </label>
+      <input type="text" id="emailAddress" name="email" placeholder="John@gmail.com" />
 
-export default LoginSingnUp
+      <br/>
+      <label htmlFor="fname">Password: </label>
+      <input type="text" id="fname" name="password" />
+
+      <br/>
+      <label htmlFor="fname">Confirm Password: </label>
+      <input type="text" id="fname" name="confirmPassword" />
+
+      <br/>
+      <input type="submit" value="Submit" />
+    </div>
+  );
+};
+
+export default LoginSignUp;
