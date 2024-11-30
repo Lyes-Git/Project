1f6dcff1ae47553544969b1606b536db2d179fe3
Danny Fu <43212622+DannyFu9993@users.noreply.github.com>
2024-11-30 15:31:32 -0500

    deleted menu from navbar because it didn't exist

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,24 +9,23 @@ const Navbar = ({ cartItems = [] }) => {
 
   return (
     <div className='navbar'>
-        <div className="nav-logo">
-            <img src={shopping_bag} alt="" />
-            <p>SHOPPERS</p>
-        </div>
-        <ul className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("rings")}}><Link to='/rings'>Rings</Link>{menu==="rings"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("neclaces")}}><Link to='/neclaces'>Neclaces</Link>{menu==="neclaces"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("earings")}}><Link to='/earings'>Earings</Link>{menu==="earings"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("bracelets")}}><Link to='/bracelets'>Bracelets</Link>{menu==="bracelets"?<hr/>:<></>}</li>
-            <hr />
-        </ul>
-        <div className="nav-login-cart">
-            <Link to='login'><button>login</button></Link>
-            <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-           <div className="nav-cart-count">0</div>
-        </div>
+      <div className="nav-logo">
+        <img src={shopping_bag} alt="" />
+        <p>SHOPPERS</p>
       </div>
+      <ul className="nav-menu">
+        <li onClick={() => { setMenu("shop") }}><Link to='/'>Shop</Link> </li>
+        <li onClick={() => { setMenu("rings") }}><Link to='/rings'>Rings</Link></li>
+        <li onClick={() => { setMenu("neclaces") }}><Link to='/neclaces'>Neclaces</Link></li>
+        <li onClick={() => { setMenu("earings") }}><Link to='/earings'>Earings</Link></li>
+        <li onClick={() => { setMenu("bracelets") }}><Link to='/bracelets'>Bracelets</Link></li><hr />
+      </ul>
+      <div className="nav-login-cart">
+        <Link to='/signup'><button>login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
     //</div>
   );
 };
