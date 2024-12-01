40725640ea5c52474f5d15bd9bcb9ed2058ce452
Danny Fu <43212622+DannyFu9993@users.noreply.github.com>
2024-11-30 23:20:15 -0500

    edited image logic for server deployment

--- src/Components/assets/data.js
+++ src/Components/assets/data.js
@@ -1,23 +1,23 @@
 let productData = {
   rings: [
-    { id: 101, name: 'Gold Ring', price: 150, img: 'public/images/rings/gold_ring.jpg' },
-    { id: 102, name: 'Silver Ring', price: 100, img: 'public/images/rings/silver_ring.jpg' },
-    { id: 103, name: 'Diamond Ring', price: 500, img: 'public/images/rings/Diamond_ring.jpg' },
+    { id: 101, name: 'Gold Ring', price: 150, img: '/images/rings/gold_ring.jpg' },
+    { id: 102, name: 'Silver Ring', price: 100, img: '/images/rings/silver_ring.jpg' },
+    { id: 103, name: 'Diamond Ring', price: 500, img: '/images/rings/Diamond_ring.jpg' },
   ],
   necklaces: [
-    { id: 201, name: 'Diamond Necklace', price: 200, img: 'public/images/necklaces/diamond necklace.webp' },
-    { id: 202, name: 'Gold Necklace', price: 350, img: 'public/images/necklaces/goldnecklace.webp' },
-    { id: 203, name: 'Silver Necklace', price: 150, img: 'public/images/necklaces/silverneck.jpg' },
+    { id: 201, name: 'Diamond Necklace', price: 200, img: '/images/necklaces/diamond necklace.webp' },
+    { id: 202, name: 'Gold Necklace', price: 350, img: '/images/necklaces/goldnecklace.webp' },
+    { id: 203, name: 'Silver Necklace', price: 150, img: '/images/necklaces/silverneck.jpg' },
   ],
   earrings: [
-    { id: 301, name: 'Gold Earrings', price: 120, img: 'public/images/earrings/gold_earring.jpg' },
-    { id: 302, name: 'Diamond Earrings', price: 300, img: 'public/images/earrings/diamond_earring.jpg' },
-    { id: 303, name: 'Silver Earrings', price: 90, img: 'public/images/earrings/silver_earrings.jpg' },
+    { id: 301, name: 'Gold Earrings', price: 120, img: '/images/earrings/gold_earring.jpg' },
+    { id: 302, name: 'Diamond Earrings', price: 300, img: '/images/earrings/diamond_earring.jpg' },
+    { id: 303, name: 'Silver Earrings', price: 90, img: '/images/earrings/silver_earrings.jpg' },
   ],
   bracelets: [
-    { id: 401, name: 'Charm Bracelet', price: 100, img: 'public/images/bracelets/charm_bracelet.jpg' },
-    { id: 402, name: 'Silver Bracelet', price: 120, img: 'public/images/bracelets/silver_bracelet.jpg' },
-    { id: 403, name: 'Gold Bracelet', price: 250, img: 'public/images/bracelets/gold_bracelet.jpg' },
+    { id: 401, name: 'Charm Bracelet', price: 100, img: '/images/bracelets/charm_bracelet.jpg' },
+    { id: 402, name: 'Silver Bracelet', price: 120, img: '/images/bracelets/silver_bracelet.jpg' },
+    { id: 403, name: 'Gold Bracelet', price: 250, img: '/images/bracelets/gold_bracelet.jpg' },
   ],
 };
 
