fdbd4ecfe8f5ac66b0d1f78d71dcd006fb1a7ba2
Zakkkkuu <cruzmarkusisaac@gmail.com>
2024-12-07 22:16:58 -0500

    update homepage (Feel free to remove)

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>New Arrival -</h2>
+        <h2>New Arrival ~ </h2>
         <p>Collection for Everyone ~</p>
         <div className="hero-right-icon">
-          <p>New Models -</p>
+          <p>New Models ~</p>
           <img src={frontImage} alt="New Collection" />
         </div>
       </div>
